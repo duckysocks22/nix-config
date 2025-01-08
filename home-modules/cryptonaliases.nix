287823487b405c5b9d3b5ebab55197207a843891
duckysocks22 <jjones213067@proton.me>
2025-01-08 17:41:03 -0500

    Adjusted 'cryptonaliases.nix'

--- home-modules/cryptonaliases.nix
+++ home-modules/cryptonaliases.nix
@@ -2,8 +2,10 @@
 {
 home = {
     shellAliases = {
-      rebuild = "sudo nixos-rebuild switch -L --flake /home/sockls/files/nix-config/";
+      rebuild = "sudo nixos-rebuild switch -L --flake /home/socks/files/nix-config/";
+      ncd = "cd /home/socks/files/nix-config/";
       atm10-start = "docker compose -f /home/socks/files/nix-config/docker/allthemods10/docker-compose.yml -p allthemods10 up -d";
+      atm10-attach = "docker attach allthemods10-mc-1";
     };
   };
 }
