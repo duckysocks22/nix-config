489c65ba714cf96155c23012ad5b67bff39e5ff7
duckysocks22 <jjones213067@proton.me>
2025-02-01 23:12:14 -0500

    Created Miku The Gathering MC Server 'docker-compose.yml'. Updated 'cryptonaliases.nix' to have 'mtg-start' and 'mtg-attach'

--- nixos-modules/cryptonaliases.nix
+++ nixos-modules/cryptonaliases.nix
@@ -3,6 +3,8 @@
   programs.bash.shellAliases = {
     rebuild = "sudo nixos-rebuild switch -L --flake /home/socks/files/nix-config/";
     atm10-start = "docker compose -f /home/socks/files/nix-config/docker/allthemods10/docker-compose.yml -p allthemods10 up -d";
+    mtg-start = "docker compose -f /home/socks/files/nix-config/docker/mikuthegathering/docker-compose.yml -p mikuthegathering up -d";
+    mtg-attach = "docker attach mikuthegathering-mc-1";
   };
 
 }
